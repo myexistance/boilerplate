ac40341831cab6fe8dc91e1260515eed85636276
myexistance <joonyoonlives@gmail.com>
2020-02-18 04:30:53 +1300

    User Schema created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://Joon:TruEday80@cluster0-siyyh.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
